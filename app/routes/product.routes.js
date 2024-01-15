ff4ba4c19171d9835d61addb772c4dd2f64ce1ed
Anoop Saxena <anoop@esoftech.com>
2024-01-15 16:24:30 +0530

    auth apis

--- app/routes/product.routes.js
+++ app/routes/product.routes.js
@@ -2,8 +2,8 @@ const { authJwt } = require("../middleware");
 const productController = require("../controllers/product.controller");
 
 module.exports = function (app) {
-    app.get('/api/product/allProductCategory', [authJwt.verifyToken], productController.getAllProductCategory);
-
-    app.post('/api/product/createProduct', [authJwt.verifyToken], productController.createProduct);
-    app.get('/api/product/subproduct/:parent_id', [authJwt.verifyToken], productController.getSubProducts);
+    // app.get('/api/products/', [authJwt.verifyToken], productController.getAllProductCategory);
+    // app.get('/api/products/:product_id', [authJwt.verifyToken], productController.createProduct);
+    // app.put('/api/products/', [authJwt.verifyToken], productController.createProduct);
+    // app.get('/api/products/category/:category_id', [authJwt.verifyToken], productController.getSubProducts);
 };
