d74e88b00f3468f8809dc4cb1bf49404de18b074
Anoop Saxena <anoop@esoftech.com>
2024-01-30 19:18:48 +0530

    prpeared swagger script

--- swagger-routes/certificates.js
+++ swagger-routes/certificates.js
@@ -1,114 +1,8 @@
 /**
  * @swagger
  * tags:
- *   name: Certificates
- *   description: Operations related to Certificate management
- */
-
-/**
- * @swagger
- * /api/certificate/:
- *   post:
- *     summary: Create a new certificate
- *     tags: [Certificates]
- *     security:
- *       - jwt: []
- *     requestBody:
- *       description: Data for creating a new certificate
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             $ref: '#/components/schemas/NewCertificate'
- *     responses:
- *       201:
- *         description: Certificate created successfully
- *         content:
- *           application/json:
- *             schema:
- *               $ref: '#/components/schemas/Certificate'
- *       500:
- *         description: Internal Server Error
-
- *   get:
- *     summary: Get all certificates
- *     tags: [Certificates]
- *     security:
- *       - jwt: []
- *     responses:
- *       200:
- *         description: List of all certificates
- *         content:
- *           application/json:
- *             schema:
- *               type: array
- *               items:
- *                 $ref: '#/components/schemas/Certificate'
- *       404:
- *         description: No certificates returned
- *       500:
- *         description: Internal Server Error
-
- * /api/certificate/{reg_id}:
- *   get:
- *     summary: Get certificate by registration ID
- *     tags: [Certificates]
- *     security:
- *       - jwt: []
- *     parameters:
- *       - in: path
- *         name: reg_id
- *         schema:
- *           type: integer
- *         required: true
- *         description: Registration ID of the certificate to retrieve
- *     responses:
- *       200:
- *         description: Certificate details retrieved successfully
- *         content:
- *           application/json:
- *             schema:
- *               $ref: '#/components/schemas/Certificate'
- *       404:
- *         description: Details not found
- *       500:
- *         description: Internal Server Error
-
- *   put:
- *     summary: Update certificate by ID
- *     tags: [Certificates]
- *     security:
- *       - jwt: []
- *     parameters:
- *       - in: path
- *         name: certificate_id
- *         schema:
- *           type: integer
- *         required: true
- *         description: ID of the certificate to update
- *     requestBody:
- *       description: Data for updating the certificate
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             $ref: '#/components/schemas/NewCertificate'
- *     responses:
- *       200:
- *         description: Certificate record updated successfully
- *         content:
- *           application/json:
- *             schema:
- *               $ref: '#/components/schemas/Certificate'
- *       404:
- *         description: Certificate not found
- *       500:
- *         description: Internal Server Error
- */
-
-// Define the components section with schemas for Certificate and NewCertificate
-/**
- * @swagger
+ *   name: Certificate
+ *   description: API for managing certificates
  * components:
  *   schemas:
  *     Certificate:
@@ -116,51 +10,125 @@
  *       properties:
  *         id:
  *           type: integer
- *           description: ID of the certificate
- *         email:
- *           type: string
- *           description: Email of the certificate
  *         name:
  *           type: string
- *           description: Name of the certificate
- *         mobile_no:
+ *         description:
  *           type: string
- *           description: Mobile number of the certificate
- *         otp:
- *           type: string
- *           description: OTP of the certificate
- *         created_by:
- *           type: string
- *           description: Created by user
- *         modified_by:
- *           type: string
- *           description: Modified by user
- *         registrationId:
+ *         reg_id:
  *           type: integer
- *           description: Registration ID associated with the certificate
- *
- *     NewCertificate:
- *       type: object
- *       properties:
- *         email:
- *           type: string
- *           description: Email of the certificate
- *         name:
- *           type: string
- *           description: Name of the certificate
- *         mobile_no:
- *           type: string
- *           description: Mobile number of the certificate
- *         otp:
- *           type: string
- *           description: OTP of the certificate
- *         created_by:
- *           type: string
- *           description: Created by user
- *         modified_by:
- *           type: string
- *           description: Modified by user
- *         registrationId:
- *           type: integer
- *           description: Registration ID associated with the certificate
+ * paths:
+ *   /api/certificate:
+ *     post:
+ *       summary: Create a new certificate
+ *       tags: [Certificate]
+ *       security:
+ *         - api_key: []
+ *       requestBody:
+ *         required: true
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Certificate'
+ *       responses:
+ *         '201':
+ *           description: Certificate created successfully
+ *         '400':
+ *           description: Bad request
+ *         '500':
+ *           description: Some server error
+ *     get:
+ *       summary: Get all certificates
+ *       tags: [Certificate]
+ *       security:
+ *         - api_key: []
+ *       responses:
+ *         '200':
+ *           description: Successful response
+ *           content:
+ *             application/json:
+ *               example:
+ *                 - id: 1
+ *                   name: Certificate1
+ *                   description: Description1
+ *                   reg_id: 123
+ *                 - id: 2
+ *                   name: Certificate2
+ *                   description: Description2
+ *                   reg_id: 456
+ *         '500':
+ *           description: Some server error
+ *   /api/certificate/{reg_id}:
+ *     get:
+ *       summary: Get a certificate by registration ID
+ *       tags: [Certificate]
+ *       parameters:
+ *         - name: reg_id
+ *           in: path
+ *           required: true
+ *           description: Registration ID
+ *           schema:
+ *             type: integer
+ *       security:
+ *         - api_key: []
+ *       responses:
+ *         '200':
+ *           description: Successful response
+ *           content:
+ *             application/json:
+ *               example:
+ *                 - id: 1
+ *                   name: Certificate1
+ *                   description: Description1
+ *                   reg_id: 123
+ *         '404':
+ *           description: Certificate not found
+ *         '500':
+ *           description: Some server error
+ *   /api/certificate/{certificate_id}:
+ *     put:
+ *       summary: Update a certificate by ID
+ *       tags: [Certificate]
+ *       parameters:
+ *         - name: certificate_id
+ *           in: path
+ *           required: true
+ *           description: Certificate ID
+ *           schema:
+ *             type: integer
+ *       security:
+ *         - api_key: []
+ *       requestBody:
+ *         required: true
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Certificate'
+ *       responses:
+ *         '200':
+ *           description: Certificate updated successfully
+ *         '400':
+ *           description: Bad request
+ *         '404':
+ *           description: Certificate not found
+ *         '500':
+ *           description: Some server error
+ *     delete:
+ *       summary: Delete a certificate by ID
+ *       tags: [Certificate]
+ *       parameters:
+ *         - name: certificate_id
+ *           in: path
+ *           required: true
+ *           description: Certificate ID
+ *           schema:
+ *             type: integer
+ *       security:
+ *         - api_key: []
+ *       responses:
+ *         '204':
+ *           description: Certificate deleted successfully
+ *         '404':
+ *           description: Certificate not found
+ *         '500':
+ *           description: Some server error
  */
