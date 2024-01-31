db9cd1a3a0f330a6057dd3f8fe80b9c898f476f1
Anoop Saxena <anoop@esoftech.com>
2024-01-31 16:59:03 +0530

    controllers checks

--- app/controllers/product.controller.js
+++ app/controllers/product.controller.js
@@ -1,6 +1,5 @@
 const db = require("../models");
 const path = require('path');
-const { Op, DataTypes, Sequelize, where } = require('sequelize');
 const Product = db.product;
 const Registration = db.registration;
 const Category = db.category;
@@ -10,7 +9,10 @@ const parseCSV = require('./csvParser');
  method to generate unique SKU in form SKU_****
  */
 function generateUniqueSKU() {
-    return 'SKU_' + Date.now().toString() + Math.floor(Math.random() * 1000);
+    const randomNumber = Math.floor(Math.random() * 10000000000); // 10-digit random number
+    const timestamp = Date.now().toString();
+    const uniqueSKU = 'SKU_' + timestamp + randomNumber.toString().padStart(6, '0');
+    return uniqueSKU;
 }
 
 
@@ -20,14 +22,14 @@ function generateUniqueSKU() {
  * @param {Object} res - Express response object.
  */
 module.exports.getAllProducts = async function (req, res) {
-    try{
+    try {
         const products = await Product.findAll({});
-        if(products){
+        if (products) {
             res.status(200).json(products);
-        }else{
-            res.status(500).json({error : 'No Product Found'});
+        } else {
+            res.status(500).json({ error: 'No Product Found' });
         }
-    }catch (error) {
+    } catch (error) {
         console.error(error);
         res.status(500).json({ message: 'Internal server error ' + error.message });
     }
@@ -41,18 +43,18 @@ module.exports.getAllProducts = async function (req, res) {
  */
 module.exports.getProductBySKU = async function (req, res) {
     const sku = req.params.sku;
-    try{
+    try {
         const products = await Product.findOne({
-            where:{
-                sku:sku
+            where: {
+                sku: sku
             }
         });
-        if(products){
+        if (products) {
             res.status(200).json(products);
-        }else{
-            res.status(500).json({error : 'No Product Found'});
+        } else {
+            res.status(500).json({ error: 'No Product Found' });
         }
-    }catch (error) {
+    } catch (error) {
         console.error(error);
         res.status(500).json({ message: 'Internal server error ' + error.message });
     }
@@ -67,31 +69,31 @@ module.exports.getProductBySKU = async function (req, res) {
  */
 module.exports.createProduct = async function (req, res) {
     try {
-        const { type, registrationId,category_id } = req.body;
+        const { type, registrationId, category_id } = req.body;
         // Split comma-separated category IDs into an array
         const categoryIdsArray = category_id.split(',');
         let categoryIds;
         //const imageFileNames = req.files['images'].map((file) => path.basename(file.path));
-        
+
         if (req.files && req.files['csvFilePath'][0].path) {
             const csvData = await parseCSV(req.files['csvFilePath'][0].path);
-            
+
             for (const row of csvData) {
                 const productImages = row.product_images ? row.product_images.split(',') : [];
                 const productImagesString = productImages.join(',');
                 let catIdArray;
                 // Split comma-separated category IDs into an array
-                const rowCategoryIdsArray = row.category_id?row.category_id.split(','):[];
-                if(rowCategoryIdsArray.length >0){
+                const rowCategoryIdsArray = row.category_id ? row.category_id.split(',') : [];
+                if (rowCategoryIdsArray.length > 0) {
                     catIdArray = rowCategoryIdsArray;
                     categoryIds = rowCategoryIdsArray.join(',');
-                }else{
+                } else {
                     catIdArray = categoryIdsArray;
                     categoryIds = categoryIdsArray.join(',');
                 }
 
                 if (row.sku) {
-                    console.log('*****', row.sku ,'**catIdArray*',catIdArray);
+                    console.log('*****', row.sku, '**catIdArray*', catIdArray);
                     const [product, created] = await Product.findOrCreate({
                         where: { sku: row.sku },
                         defaults: {
@@ -101,7 +103,7 @@ module.exports.createProduct = async function (req, res) {
                             registrationId,
                             budget: row.budget,
                             moq: row.moq,
-                            category_id:categoryIds,
+                            category_id: categoryIds,
                             //images: imageFileNames,
                             default_image: row.default_image,
                             product_images: productImagesString
@@ -154,10 +156,10 @@ module.exports.createProduct = async function (req, res) {
  * @param {Object} req - Express request object.
  * @param {Object} res - Express response object.
  */
-module.exports.createProductsImages = async function(req,res){
-    try{
-        res.status(200).json({message: 'Images Uploaded Successfully'});
-    }catch (error) {
+module.exports.createProductsImages = async function (req, res) {
+    try {
+        res.status(200).json({ message: 'Images Uploaded Successfully' });
+    } catch (error) {
         console.error(error);
         res.status(500).json({ message: 'Internal server error ' + error.message });
     }
@@ -169,9 +171,9 @@ module.exports.createProductsImages = async function(req,res){
  * @param {Object} req - Express request object.
  * @param {Object} res - Express response object.
  */
-module.exports.createProductsSingleRecord = async function(req,res){
-    try{
-        const { type, registrationId,title,description,budget,moq,category_id } = req.body;
+module.exports.createProductsSingleRecord = async function (req, res) {
+    try {
+        const { type, registrationId, title, description, budget, moq, category_id } = req.body;
         const imageFileNames = req.files['images'].map((file) => path.basename(file.path));
         const sku = generateUniqueSKU();
         const productImagesString = imageFileNames.join(',');
@@ -184,31 +186,31 @@ module.exports.createProductsSingleRecord = async function(req,res){
             type: type,
             registrationId,
             category_id: categoryIdsArray.join(','),
-            budget:budget,
-            moq:moq,
+            budget: budget,
+            moq: moq,
             default_image: imageFileNames[0],
             product_images: productImagesString
         });
         const categories = await Category.findAll({
             where: {
-              id: categoryIdsArray,
+                id: categoryIdsArray,
             },
         });
-      
+
         // Associate the product with categories
         await product.addCategories(categories);
         // update the business_type on Registration
         await Registration.update({ business_type: type },
             { where: { id: registrationId } }
         );
-      
-        if(product){
+
+        if (product) {
             res.status(200).json(product);
-        }else{
-            res.status(400).json({error : 'Product not inserted'});
+        } else {
+            res.status(400).json({ error: 'Product not inserted' });
         }
 
-    }catch (error) {
+    } catch (error) {
         console.error(error);
         res.status(500).json({ message: 'Internal server error ' + error.message });
     }
@@ -220,124 +222,111 @@ module.exports.createProductsSingleRecord = async function(req,res){
  * @param {Object} req - Express request object.
  * @param {Object} res - Express response object.
  */
-module.exports.updateProducts = async function(req,res){
-    try{
+module.exports.updateProducts = async function (req, res) {
+    try {
         const sku = req.params.sku;
-        const { title,description,budget,moq,category_id } = req.body;
-        if(!category_id){
-            return res.status(401).json({error: 'No category Provided'});
+        const { title, description, budget, moq, category_id } = req.body;
+        if (!category_id) {
+            return res.status(401).json({ error: 'No category Provided' });
         }
         // Split comma-separated category IDs into an array
         const categoryIdsArray = category_id.split(',');
         const categories = await Category.findAll({
             where: {
-              id: categoryIdsArray,
+                id: categoryIdsArray,
             },
         });
-        if(categories.length==0){
-            return res.status(401).json({error: 'No category with this id'});
+        if (categories.length == 0) {
+            return res.status(401).json({ error: 'No category with this id' });
         }
-        let catArray =[];
-        categories.forEach((cat)=>{
+
+        let catArray = [];
+        categories.forEach((cat) => {
             catArray.push(cat.id)
         });
+
         let productImagesString;
         let imageFileNames;
-        if(req.files['images']){ // if images are uploaded then then update product_images and default_image field
+
+        const product_details = {
+            title,
+            description,
+            budget,
+            moq,
+            category_id: catArray.join(',')
+        }
+
+        if (req.files['images']) { // if images are uploaded then then update product_images and default_image field
             imageFileNames = req.files['images'].map((file) => path.basename(file.path));
             productImagesString = imageFileNames.join(',');
-            const [rowUpdated, productUpdated] = await Product.update({
-                title,
-                description,
-                budget,
-                moq,
-                category_id: catArray.join(','),
-                default_image: imageFileNames[0],
-                product_images: productImagesString
-            }, {
-                where: {
-                    sku: sku
-                },
-                returning: true
-            });
-            
-            if(rowUpdated>0){
-                // Update associations
-                await productUpdated[0].setCategories(categories);
-                res.status(200).json(productUpdated[0]);
-            }else{
-                res.status(404).json({error : 'No Product found with this sku'});
-            }
-        }else{ // here product_images and default_image are not updated
-            const [rowUpdated, productUpdated] = await Product.update({
-                title,
-                description,
-                budget,
-                moq,
-                category_id: catArray.join(','),
-            }, {
-                where: {
-                    sku: sku
-                },
-                returning: true
-            });
-            if(rowUpdated>0){
-                // Update associations
-                await productUpdated[0].setCategories(categories);
-                res.status(200).json(productUpdated[0]);
-            }else{
-                res.status(404).json({error : 'No Product found with this sku'});
-            }
+            product_details.default_image = imageFileNames[0];
+            product_details.product_images = productImagesString
         }
-    }catch(err){
-        res.status(500).json({error: 'Internal Server Error '+err.message})
+
+        const [rowUpdated, productUpdated] = await Product.update(product_details, {
+            where: {
+                sku: sku
+            },
+            returning: true
+        });
+
+        if (rowUpdated > 0) {
+            // Update associations
+            await productUpdated[0].setCategories(categories);
+            res.status(200).json(productUpdated[0]);
+        } else {
+            res.status(404).json({ error: 'No Product found with this sku' });
+        }
+
+    } catch (err) {
+        res.status(500).json({ error: 'Internal Server Error ' + err.message })
     }
 }
 
 module.exports.getProductByRegistrationId = async function (req, res) {
     const registrationId = req.params.registrationId;
-    try{
+    try {
         const products = await Product.findAll({
-            where:{
+            where: {
                 registrationId: registrationId
             }
         });
-        if(products.length>0){
+        if (products.length > 0) {
             res.status(200).json(products);
-        }else{
-            res.status(500).json({error : 'No Product Found'});
+        } else {
+            res.status(500).json({ error: 'No Product Found' });
         }
-    }catch (error) {
+    } catch (error) {
         console.error(error);
         res.status(500).json({ message: 'Internal server error ' + error.message });
     }
 }
 
-module.exports.deleteProductBySku = async function(req,res){
-    try{
+module.exports.deleteProductBySku = async function (req, res) {
+    try {
         const sku = req.params.sku;
         const productToDelete = await Product.findOne({
-            where:{
-                sku:sku
+            where: {
+                sku: sku
             }
         });
-        if(!productToDelete){
-            return res.status(404).json({error : 'No Product found'});
+        if (!productToDelete) {
+            return res.status(404).json({ error: 'No Product found' });
         }
         const deletedProduct = await Product.destroy({
             where: {
-                sku:sku
+                sku: sku
             },
             returning: true,
             raw: true,
         });
-        if(deletedProduct){
-            return res.status(200).json({message:'Product Deleted Successfully',product: productToDelete });
-        }else{
-            return res.status(401).json({ error : 'No Product Deleted'});
+        if (deletedProduct) {
+            return res.status(200).json({ message: 'Product Deleted Successfully', product: productToDelete });
+        } else {
+            return res.status(401).json({ error: 'No Product Deleted' });
         }
-        
-    }catch(err){
-        res.status(500).json({error: 'Internal Server Error '+err.message});
+
+    } catch (err) {
+        res.status(500).json({ error: 'Internal Server Error ' + err.message });
     }
 }
