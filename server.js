8aa31238f28f9e48dd6da91592025fcae6aa265a
Anoop Saxena <anoop@esoftech.com>
2024-02-12 13:29:47 +0530

    server.js

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const swaggerOptions = require("./swagger-routes/swagger");  // Adjust the path 
 const initializeRoutes = require("./app/routes/routeInitializer")
 const bodyParser = require('body-parser')
 require('dotenv').config();
+require('express-async-errors');
 
 const app = express();
 app.use(
@@ -42,6 +43,19 @@ app.get("/", (req, res) => {
 //Swagger options
 const specs = swaggerJsdoc(swaggerOptions);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
+
+
+//Error handling
+
+app.use((err, req, res, next) => {
+  if (err.message === 'access denied') {
+    res.status(403);
+    res.json({ error: err.message });
+  }
+ 
+  next(err);
+});
+
 
 // set port, listen for requests
 const PORT = process.env.PORT;
