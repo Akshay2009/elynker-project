c5756a1914c538eed58098d5ccfcac63b306cba7
Anoop Saxena <anoop@esoftech.com>
2024-04-05 14:07:15 +0530

    file size fixed

--- server.js
+++ server.js
@@ -31,8 +31,8 @@ app.use(function(req, res, next) {
 });
 
 app.use('/uploads', express.static(__dirname+'/uploads'));
-app.use(bodyParser.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 //Initialized app to models
 initializeRoutes(app);
